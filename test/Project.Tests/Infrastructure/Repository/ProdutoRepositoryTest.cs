cf2a95a9870a8cf01f56435a8b402369d19f1bb8
Brum <fbrump@gmail.com>
2017-08-11 00:15:51 -0300

    First class for TDD

---
+++ test/Project.Tests/Infrastructure/Repository/ProdutoRepositoryTest.cs
@@ -0,0 +1,15 @@
+namespace Project.Tests.Infrastructure.Repository
+{
+    using System;
+    using Xunit;
+    public class ProdutoRepositoryTest
+    {
+        
+        [Fact]
+        public void Should_inser_new_product()
+        {
+
+        }
+
+    }
+}
