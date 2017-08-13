d276d38487a17807632f66a3453fbfbf2cb2fc80
Brum <fbrump@gmail.com>
2017-08-13 01:46:22 -0300

    Starting configurations database for UnitTest and install package Microsfot.Configuration.Extenssion.Json 1.1.2

---
+++ test/Project.Tests/Startup.cs
@@ -0,0 +1,17 @@
+namespace Project.Tests
+{
+    public class Startup
+    {
+        // public Startup(IHostingEnvironment env)
+        // {
+        //     var builder = new ConfigurationBuilder()
+        //         .SetBasePath(env.ContentRootPath)
+        //         .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        //         .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+        //         .AddEnvironmentVariables();
+        //     Configuration = builder.Build();
+        // }
+
+        // public IConfigurationRoot Configuration { get; }
+    }
+}
