747dcd7d6eb94ccdc9a6bc25543625758d8562ea
Brum <fbrump@gmail.com>
2017-08-13 22:23:02 -0300

    Changed entity product change one property and added new property.

--- src/Project.Infrastructure/Entity/Produto.cs
+++ src/Project.Infrastructure/Entity/Produto.cs
@@ -5,6 +5,7 @@ namespace Project.Infrastructure.Entity
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Categoria { get; set; }
-        public double Preco { get; set; }
+        public double PrecoUnitario { get; set; }
+        public string Unidade { get; set; }        
     }
 }
